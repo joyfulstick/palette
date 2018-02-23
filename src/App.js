7a32d41e5cf3063eb23a63269afa629a76f4ab7d
joyfulstick <m.kijewicz@gmail.com>
2018-02-23 09:00:28 +0100

    creating hsv picker

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Hsv from './components/Hsv'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Hsv />
+      </div>
+    )
+  }
+}
+
+export default App
